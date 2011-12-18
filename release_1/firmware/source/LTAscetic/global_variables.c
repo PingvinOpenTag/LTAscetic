06f68defd28c55d3a4a74cbad26b6c2eb3840c96
PingvinOpenTag <elektron_pochta@mail.ru>
2011-12-18 20:09:16 +0300

    Update release_1/firmware/source/LTAscetic/global_variables.c

--- release_1/firmware/source/LTAscetic/global_variables.c
+++ release_1/firmware/source/LTAscetic/global_variables.c
@@ -1,61 +1,61 @@
 #include <avr/io.h>        // Определяет имена для портов ввода-ывода
-#include <util/delay.h>    // ���� ����������� ������������ ��������
-#include <avr/interrupt.h> // ����� ������������ ����������
-#include <avr/pgmspace.h>  //����� ������� ��������� � ������ ��������
+#include <util/delay.h>    // Дает возможность формирования задержки
+#include <avr/interrupt.h> // Будем использовать прерывания
+#include <avr/pgmspace.h>  //Будем хранить константы в памяти программ
 #include "types.h"
 volatile uint16_t timer1; 
 
 
-volatile bool start_bit_received;				//���� ���� ��������������, ���� ������ �����-���
+volatile bool start_bit_received;				//Этот флаг устанвливается, если принят старт-бит
 
-volatile uint16_t high_level_counter; 			//������� ������������ ������� �������� ������ �� ������ ��-���������										
-												//������� ����� �������� ���������
-
-
-volatile uint16_t bit_in_rx_buff; 				//���������� ���, �������� ��-���������� 
-
-volatile trx_event rx_event; 					//������� ��-���������
-
-volatile uint16_t low_level_counter; 			//������� ������������ ������� ������� ������ �� ������ ��-���������										
-												//������� ����� �������� ���������
+volatile uint16_t high_level_counter; 			//Счетчик длительности сигнала высокого уровня на выводе ИК-приемника										
+												//который нужно передать следующим
 
 
-volatile uint8_t rx_buffer[RX_BUFFER_SIZE]; 	//������ ��-���������
+volatile uint16_t bit_in_rx_buff; 				//Количество бит, принятых ИК-приемником 
 
-volatile uint8_t tx_buffer[TX_BUFFER_SIZE]; 	//������ ��-���������
+volatile trx_event rx_event; 					//события ИК-приемника
 
-
-volatile bool ir_transmitter_on;	//����, ����������� (true) ��� ����������� �������� ������ ����� ��-���� 
-
-volatile int ir_pulse_counter; 					//�������� ������� "�������" ��-�����
-
-volatile int ir_space_counter; 					//�������� ������� ������������ ������������ ��������� ��-����� (����� ����� ������) 
-
-volatile union data_packet_union  data_packet; 	//� ���� ���������� ����� ����������� ����� ������ ��� �������� ����� IR
+volatile uint16_t low_level_counter; 			//Счетчик длительности сигнала низкого уровня на выводе ИК-приемника										
+												//который нужно передать следующим
 
 
-volatile uint8_t cursor_position;				//��� ���������� ����� ������� ����� �������� ������� ������ data_packet.data[x]
+volatile uint8_t rx_buffer[RX_BUFFER_SIZE]; 	//Буффер ИК-приемника
 
-volatile union data_packet_union  data_packet; 	//� ���� ���������� ����� ����������� ����� ������ ��� �������� ����� IR
+volatile uint8_t tx_buffer[TX_BUFFER_SIZE]; 	//Буффер ИК-приемника
+
+
+volatile bool ir_transmitter_on;	//флаг, разрешаюший (true) или запрещающий передачу данных через ИК-диод 
+
+volatile int ir_pulse_counter; 					//Обратный счетчик "вспышек" ИК-диода
+
+volatile int ir_space_counter; 					//Обратный счетчик длительности выключенного состояния ИК-диода (пауза между битами) 
+
+volatile union data_packet_union  data_packet; 	//В этой переменной будем формировать пакет данных для отправки через IR
+
+
+volatile uint8_t cursor_position;				//Эта переменная будет хранить индех элемента массива данных data_packet.data[x]
+
+volatile union data_packet_union  data_packet; 	//В этой переменной будем формировать пакет данных для отправки через IR
 
 volatile trx_packet rx_packet;
 
-volatile uint8_t life; //������� ����� (��������)
+volatile uint8_t life; //уровень жизни (здоровье)
 
-volatile uint8_t life_leds_status[4]; //���� ������ ����� ������� ��������� 
+volatile uint8_t life_leds_status[4]; //этот массив будет хранить состояния 
 
-volatile TKEYBOARD_EVENT  keyboard_event; //������� ���������� 
+volatile TKEYBOARD_EVENT  keyboard_event; //события клавиатуры 
 
-volatile struct pressing_duration key_pressing_duration;//��������� ������ �������� ������������ ������� ������
+volatile struct pressing_duration key_pressing_duration;//структура хранит счетчики длительности нажатия кнопок
 
-volatile uint8_t fire_led_status; //������ ���������� ������� �������� 
+volatile uint8_t fire_led_status; //статус светодиода вспышки выстрела 
 
-volatile uint16_t bullets;  //���������� �������� � ����
+volatile uint16_t bullets;  //количество патронов в таге
 
-volatile uint16_t last_simple;					//���������� ����� ��������� ������� �����
+volatile uint16_t last_simple;					//порядковый номер последней выборки звука
 
 
-//������ ��� ��������� ��������� �������� �����. �������� ���� = damage_value[Damage_xx]
+//Массив для получения реального значения урона. Реальный урон = damage_value[Damage_xx]
 uint8_t damage_value [] PROGMEM = 
 {
 	1,   // 0000 = 1   (Damage_1)
