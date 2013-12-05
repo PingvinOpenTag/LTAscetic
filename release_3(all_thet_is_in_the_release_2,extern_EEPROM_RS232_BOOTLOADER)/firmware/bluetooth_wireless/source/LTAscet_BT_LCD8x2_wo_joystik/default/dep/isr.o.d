c4c8592eb05a5a76f29ab7d3b36358c3de676f83
Андрей Чубара <adnrey@sapr-spec.sibgipro.ru>
2013-12-05 13:55:20 +0700

    Added wireless firmware

---
+++ release_3(all_thet_is_in_the_release_2,extern_EEPROM_RS232_BOOTLOADER)/firmware/bluetooth_wireless/source/LTAscet_BT_LCD8x2_wo_joystik/default/dep/isr.o.d
@@ -0,0 +1,71 @@
+isr.o: ../isr.c ../ltag_ascetic.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/io.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  d:/winavr/lib/gcc/../../avr/include/inttypes.h \
+  d:/winavr/lib/gcc/../../avr/include/stdint.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/iom32.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/portpins.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/common.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/version.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/fuse.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/lock.h \
+  d:/winavr/lib/gcc/../../avr/include/util/delay.h \
+  d:/winavr/lib/gcc/../../avr/include/util/delay_basic.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/interrupt.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h \
+  d:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  d:/winavr/lib/gcc/../../avr/include/avr/eeprom.h \
+  d:/winavr/lib/gcc/../../avr/include/string.h \
+  ../definition_of_ports_atmega16.h ../hal.h ../miles_protocol.h \
+  ../types.h ../joystick_driver_types.h ../usart.h \
+  d:/winavr/lib/gcc/../../avr/include/util/setbaud.h
+
+../ltag_ascetic.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/io.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+d:/winavr/lib/gcc/../../avr/include/inttypes.h:
+
+d:/winavr/lib/gcc/../../avr/include/stdint.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/iom32.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/portpins.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/common.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/version.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/fuse.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/lock.h:
+
+d:/winavr/lib/gcc/../../avr/include/util/delay.h:
+
+d:/winavr/lib/gcc/../../avr/include/util/delay_basic.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/interrupt.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+d:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+d:/winavr/lib/gcc/../../avr/include/avr/eeprom.h:
+
+d:/winavr/lib/gcc/../../avr/include/string.h:
+
+../definition_of_ports_atmega16.h:
+
+../hal.h:
+
+../miles_protocol.h:
+
+../types.h:
+
+../joystick_driver_types.h:
+
+../usart.h:
+
+d:/winavr/lib/gcc/../../avr/include/util/setbaud.h:
